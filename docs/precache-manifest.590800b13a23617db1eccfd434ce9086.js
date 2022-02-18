3e39eefd26b6bba0d0363d845b338359d2bb3991
David Gallegos <dgallegosv94@gmail.com>
2022-02-18 17:24:02 -0600

    Se modifica el folder name para subir a GhubPages

---
+++ docs/precache-manifest.590800b13a23617db1eccfd434ce9086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d90722f306da42d79dc3c909acd6c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "762032c3a19172fb2d7d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762032c3a19172fb2d7d",
+    "url": "/static/js/main.6dead254.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
